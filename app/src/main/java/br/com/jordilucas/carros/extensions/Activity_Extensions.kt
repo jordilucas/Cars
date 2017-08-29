f6629bad68c0c429d28bf8876940eb470a3b107e
Jordi <jordilucas3@gmail.com>
2017-08-29 14:13:57 -0300

    Metodo para configurar toolbar

--- app/src/main/java/br/com/jordilucas/carros/extensions/Activity_Extensions.kt
+++ app/src/main/java/br/com/jordilucas/carros/extensions/Activity_Extensions.kt
@@ -2,7 +2,10 @@ package br.com.jordilucas.carros.extensions
 
 import android.app.Activity
 import android.support.annotation.IdRes
+import android.support.annotation.StringRes
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.view.View
 import android.widget.Toast
 
@@ -21,6 +24,20 @@ class Activity_Extensions {
     fun Activity.toast(message: CharSequence, length: Int = Toast.LENGTH_LONG) =
             Toast.makeText(this, message, length).show()
 
+    //Show Toast
+    fun Activity.toast(@StringRes message: Int, length: Int = Toast.LENGTH_SHORT) =
+            Toast.makeText(this, message, length).show()
 
+    //Configura toolbar
+    fun AppCompatActivity.setupToolbar(@IdRes id: Int, title: String? = null,
+                                       upNavigation: Boolean = false) : ActionBar {
+        val toolbar = findViewById<Toolbar>(id)
+        setSupportActionBar(toolbar)
+        if(title != null){
+            supportActionBar?.title = title
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(upNavigation)
+        return supportActionBar!!
+    }
 
 }
