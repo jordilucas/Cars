fc226e639049fc6334af20f32222727c773857ba
Jordi <jordilucas3@gmail.com>
2017-08-29 14:29:01 -0300

    setup toolbar

--- app/src/main/java/br/com/jordilucas/carros/activity/MainActivity.kt
+++ app/src/main/java/br/com/jordilucas/carros/activity/MainActivity.kt
@@ -1,13 +1,14 @@
 package br.com.jordilucas.carros.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.jordilucas.carros.R
+import br.com.jordilucas.carros.extensions.setupToolbar
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar(R.id.toolbar)
     }
 }
