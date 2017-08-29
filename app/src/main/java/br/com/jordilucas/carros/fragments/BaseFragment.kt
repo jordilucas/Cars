21c8b19a4720c3faa3e329fe78174a2e64231ea1
Jordi <jordilucas3@gmail.com>
2017-08-29 11:15:44 -0300

    Tornando a classe BaseFragment passivel a heranca

--- app/src/main/java/br/com/jordilucas/carros/fragments/BaseFragment.kt
+++ app/src/main/java/br/com/jordilucas/carros/fragments/BaseFragment.kt
@@ -1,7 +1,10 @@
 package br.com.jordilucas.carros.fragments
 
+import android.support.v4.app.Fragment
+
 /**
  * Created by jordi on 29/08/17.
  */
-class BaseFragment {
+open class BaseFragment : Fragment() {
+
 }
