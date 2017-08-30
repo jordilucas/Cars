ec4f888c4915b70f8e07e32f2017088b5327b73a
Jordi <jordilucas3@gmail.com>
2017-08-30 14:07:32 -0300

    Enum para retornar tipo do carro cadastrado

---
+++ app/src/main/java/br/com/jordilucas/carros/utils/TipoCarro.kt
@@ -0,0 +1,12 @@
+package br.com.jordilucas.carros.utils
+
+import br.com.jordilucas.carros.R
+
+/**
+ * Created by jordi on 30/08/17.
+ */
+enum class TipoCarro(val string: Int){
+    classicos(R.string.classicos),
+    esportivos(R.string.esportivos),
+    luxo(R.string.luxo)
+}
