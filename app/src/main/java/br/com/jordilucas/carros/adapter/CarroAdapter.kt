fdd7bb0087c9b8384bc47833700694d03835215c
Jordi <jordilucas3@gmail.com>
2017-09-05 21:54:00 -0300

    Refatação para usar kotlin extensions

--- app/src/main/java/br/com/jordilucas/carros/adapter/CarroAdapter.kt
+++ app/src/main/java/br/com/jordilucas/carros/adapter/CarroAdapter.kt
@@ -10,7 +10,9 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import br.com.jordilucas.carros.R
 import br.com.jordilucas.carros.domain.Carro
+import br.com.jordilucas.carros.extensions.loadUrl
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.adapter_carro.view.*
 
 /**
  * Created by jordi on 31/08/17.
@@ -28,39 +30,20 @@ class CarroAdapter(val carros:List<Carro>, val onClick:(Carro) -> Unit) :
     }
 
     override fun onBindViewHolder(holder: CarrosViewHolder, position: Int) {
-        val context = holder.itemView.context
-
+        val view = holder.itemView
         val carro = carros[position]
 
-        holder.tnome.text = carro.nome
-        holder.progress.visibility = View.VISIBLE
+        view.tNome.text = carro.nome
+        view.progress.visibility = View.VISIBLE
 
-        Picasso.with(context).load(carro.urlFoto).fit().into(holder.img,
-                object : com.squareup.picasso.Callback{
-                    override fun onSuccess() {
-                        holder.progress.visibility = View.GONE
-                    }
+        view.img.loadUrl(carro.urlFoto, view.progress)
 
-                    override fun onError() {
-                        holder.progress.visibility = View.GONE
-                    }
-                })
-        holder.itemView.setOnClickListener{ onClick(carro)}
+        view.setOnClickListener{ onClick(carro)}
 
     }
 
     class CarrosViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        var tnome: TextView
-        var img: ImageView
-        var progress: ProgressBar
-        var cardView: CardView
 
-        init {
-            tnome = view.findViewById<TextView>(R.id.tNome)
-            img = view.findViewById<ImageView>(R.id.img)
-            progress = view.findViewById<ProgressBar>(R.id.progress)
-            cardView = view.findViewById<CardView>(R.id.card_view)
-        }
     }
 
     override fun getItemCount() = this.carros.size
