1c846048e56b819c73788ee0e94b0013445a6ace
Jordi Lucas <jordilucas3@gmail.com>
2017-09-19 15:21:41 -0300

    Internal activity

--- app/src/main/java/br/com/jordilucas/carros/activity/CarroActivity.kt
+++ app/src/main/java/br/com/jordilucas/carros/activity/CarroActivity.kt
@@ -1,13 +1,31 @@
 package br.com.jordilucas.carros.activity
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
 import br.com.jordilucas.carros.R
+import br.com.jordilucas.carros.domain.Carro
+import br.com.jordilucas.carros.extensions.loadUrl
+import br.com.jordilucas.carros.extensions.setupToolbar
+import kotlinx.android.synthetic.main.activity_carro_contents.*
 
-class CarroActivity : AppCompatActivity() {
+class CarroActivity : BaseActivity() {
+
+    var carro: Carro? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carro)
+
+        carro = intent.getSerializableExtra("carro") as Carro
+        setupToolbar(R.id.toolbar, carro?.nome, true)
+
+        initViews()
+
     }
+
+    fun initViews(){
+        tDesc.text = carro?.desc
+        img.loadUrl(carro?.urlFoto)
+    }
+
 }
