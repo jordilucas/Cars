b2aeffb61a292a42449871983584833bc8478c0e
Jordi Lucas <jordilucas3@gmail.com>
2017-09-19 11:02:39 -0300

    Internal activity

---
+++ app/src/main/java/br/com/jordilucas/carros/activity/CarroActivity.kt
@@ -0,0 +1,13 @@
+package br.com.jordilucas.carros.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.jordilucas.carros.R
+
+class CarroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carro)
+    }
+}
