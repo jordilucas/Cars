7dd26c765928f793d6b3509a3d1887668ca3cb22
Jordi Lucas <jordilucas3@gmail.com>
2017-10-04 16:48:17 -0300

    Funções para fazer parser de json

--- app/src/main/java/br/com/jordilucas/carros/extensions/Json.kt
+++ app/src/main/java/br/com/jordilucas/carros/extensions/Json.kt
@@ -1,5 +1,22 @@
 package br.com.jordilucas.carros.extensions
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+
 /**
  * Created by jordi on 20/09/17.
  */
+fun Any.toJson(prettyPrinting: Boolean = false): String{
+    val builder = GsonBuilder()
+    if(prettyPrinting){
+        builder.setPrettyPrinting()
+    }
+    val json = builder.create().toJson(this)
+    return json
+}
+
+inline fun <reified T> Any.fromJson(json: String): T{
+    val type = object: TypeToken<T>() {}.type
+    return Gson().fromJson<T>(json, type)
+}
