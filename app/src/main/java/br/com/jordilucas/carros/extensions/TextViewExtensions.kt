24a888df2bf374f35b3d4e3c951e39e915345290
Jordi Lucas <jordilucas3@gmail.com>
2017-10-05 22:15:59 -0300

    TextView extensions

--- app/src/main/java/br/com/jordilucas/carros/extensions/TextViewExtensions.kt
+++ app/src/main/java/br/com/jordilucas/carros/extensions/TextViewExtensions.kt
@@ -1,5 +1,11 @@
 package br.com.jordilucas.carros.extensions
 
+import android.widget.TextView
+
 /**
  * Created by jordi on 05/10/17.
  */
+var TextView.string: String
+get() = text.toString()
+set(value){text = value}
+fun TextView.isEmpty() = text.trim().isEmpty()
