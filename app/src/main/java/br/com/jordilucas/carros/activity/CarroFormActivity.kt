1f47f383636e800ba022a776eb57fcc44273d21b
Jordi Lucas <jordilucas3@gmail.com>
2017-10-10 17:35:40 -0300

    Implementacao da tela edição de carros

--- app/src/main/java/br/com/jordilucas/carros/activity/CarroFormActivity.kt
+++ app/src/main/java/br/com/jordilucas/carros/activity/CarroFormActivity.kt
@@ -1,5 +1,8 @@
 package br.com.jordilucas.carros.activity
 
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -8,6 +11,7 @@ import br.com.jordilucas.carros.domain.Carro
 import br.com.jordilucas.carros.domain.CarroService
 import br.com.jordilucas.carros.extensions.*
 import br.com.jordilucas.carros.domain.TipoCarro
+import br.com.jordilucas.carros.utils.CameraHelper
 import kotlinx.android.synthetic.main.activity_carro_form.*
 import kotlinx.android.synthetic.main.activity_carro_form_content.*
 import org.jetbrains.anko.doAsync
@@ -16,15 +20,23 @@ import org.jetbrains.anko.uiThread
 class CarroFormActivity : BaseActivity() {
 
     val carro: Carro? by lazy { intent.getParcelableExtra<Carro>("carro") }
+    val camera = CameraHelper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carro_form)
         setupToolbar(R.id.toolbar, carro?.nome?: getString(R.string.novo_carro), true)
         initViews()
+        camera.init(savedInstanceState)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        camera.onSaveInstanceState(outState)
     }
 
     fun initViews(){
+        appBarImg.onClick { onCLickAppBarImg() }
         carro?.apply {
             appBarImg.loadUrl(carro?.urlFoto)
             tDesc.string = desc
@@ -59,6 +71,9 @@ class CarroFormActivity : BaseActivity() {
             return
         }
 
+        val dialog = ProgressDialog.show(this, "Download", "Salvando o carro, aguarde...",
+                false, true)
+
         doAsync {
             val c = carro?:Carro()
             c.nome = tNome.string
@@ -69,10 +84,39 @@ class CarroFormActivity : BaseActivity() {
                 R.id.tipoEsportivo -> TipoCarro.esportivos.name
                 else -> TipoCarro.luxo.name
             }
+
+            val file = camera.file
+            if(file != null && file.exists()){
+                val response = CarroService.postFoto(file)
+                if(response!!.isOK()){
+                    c.urlFoto = response!!.url
+                }
+            }
+
             val response = CarroService.save(c)
             uiThread {
                 toast(response!!.msg)
+                dialog.dismiss()
                 finish()
+            }
+        }
+    }
+
+    fun onCLickAppBarImg(){
+        val ms = System.currentTimeMillis()
+        val fileName = if(carro != null)
+            "foto_carro_${carro?.id}.jpg" else "foto_carro_${ms}.jpg"
+        val intent = camera.open(this, fileName)
+        startActivityForResult(intent, 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            val bitmap = camera.getBitmap(600,600)
+            if(bitmap != null){
+                camera.save(bitmap)
+                appBarImg.setImageBitmap(bitmap)
             }
         }
     }
